a044dd3048576c60e411edf2c8d04ab0d0778047
mahedihsharif <mahedi5061@gmail.com>
2024-03-30 00:51:11 +0600

    make fun of array

---
+++ arr.js
@@ -0,0 +1,18 @@
+const arr = [10, 20, 30];
+arr[3] = 50;
+
+arr[99] = 100;
+
+// console.log(arr);
+
+const names = [
+  "Mahedi H Sharif",
+  "Wasif Amin Rahil",
+  "Raiyan Abdullah",
+  "Leo Messi",
+  "Sakib al Hasan",
+];
+
+names[4] = "Sopno";
+
+console.log(names);
